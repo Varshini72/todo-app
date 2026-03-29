212dcd5fd9263f3a211a1c5f7c0aa00f4a9bb3b3
Varshini M <varshini.munipalli7@gmail.com>
2026-03-29 23:45:48 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
-app.use(express.json());
 app.use(express.static('public'));
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect('mongodb://mongo:27017/todo');
 
+// MongoDB connection
+mongoose.connect('mongodb://mongo-service:27017/todo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log("Mongo error:", err));
+// Model
 const Task = mongoose.model('Task', {
     text: String
 });
+
+// Routes
 
 // Add task
 app.post('/add', async (req, res) => {
@@ -18,7 +30,7 @@ app.post('/add', async (req, res) => {
     res.send("Task added");
 });
 
-// Get all tasks
+// Get tasks
 app.get('/tasks', async (req, res) => {
     const tasks = await Task.find();
     res.json(tasks);
@@ -30,4 +42,10 @@ app.delete('/delete/:id', async (req, res) => {
     res.send("Deleted");
 });
 
-app.listen(3000, () => console.log("Server running on 3000"));
+// Serve frontend (IMPORTANT)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Start server (ONLY ONCE ✅)
+app.listen(3000, () => console.log("Server running on port 3000"));
